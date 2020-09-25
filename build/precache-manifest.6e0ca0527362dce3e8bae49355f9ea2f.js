b720efa078504e0dae0d3100be5a6a31a7b0b718
unknown <jotheeswaraneee@gmail.com>
2020-09-25 13:36:59 +0530

    adding files for github pages

---
+++ build/precache-manifest.6e0ca0527362dce3e8bae49355f9ea2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a446e65c819804233ad7767c82119a52",
+    "url": "/monsters-rodolex/index.html"
+  },
+  {
+    "revision": "6194baaf7a1b79d88afc",
+    "url": "/monsters-rodolex/static/css/main.7e423587.chunk.css"
+  },
+  {
+    "revision": "c2e7a13dfbd436f509b1",
+    "url": "/monsters-rodolex/static/js/2.63a27bcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rodolex/static/js/2.63a27bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6194baaf7a1b79d88afc",
+    "url": "/monsters-rodolex/static/js/main.793cddbe.chunk.js"
+  },
+  {
+    "revision": "2c6aca46e2aadf10a202",
+    "url": "/monsters-rodolex/static/js/runtime-main.92d44d1f.js"
+  }
+]);
